f339a5e11adf2e168a63a73b82cacadae16ca8ac
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-05-17 20:54:24 -0400

    Implemented the updated of the color of a FoodCategory object at lib/screens/edit_food_category_screen.dart

--- lib/screens/edit_food_category_screen.dart
+++ lib/screens/edit_food_category_screen.dart
@@ -58,6 +58,8 @@ class _EditFoodCategoryScreenState extends State<EditFoodCategoryScreen> {
     _color = widget.color;
   }
 
+  void changeColor(Color color) => setState(() => _color = color);
+
   @override
   Widget build(BuildContext context) {
     AppData appData = Provider.of<AppData>(context, listen: true);
@@ -68,6 +70,21 @@ class _EditFoodCategoryScreenState extends State<EditFoodCategoryScreen> {
 
     Color primaryColor = Theme.of(context).primaryColor;
     Color accentColor = Theme.of(context).accentColor;
+
+    var foregroundColor = _color.computeLuminance() > 0.4 ? Colors.black : Colors.white;
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      onPrimary: foregroundColor,
+      primary: _color,
+      elevation: 3,
+      textStyle: TextStyle(
+        color: Colors.red,
+      ),
+      minimumSize: Size(double.infinity, 36),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(2)),
+      ),
+    );
 
     return SafeArea(
       bottom: false,
@@ -130,6 +147,53 @@ class _EditFoodCategoryScreenState extends State<EditFoodCategoryScreen> {
                     });
                   },
                   onFieldSubmitted: _hasValidData() ? (_) => () => _updateData(context, onUpdateFoodCategoriesHandler) : null,
+                ),
+
+                // Color Input:
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: ElevatedButton(
+                    // elevation: 3.0,
+                    style: raisedButtonStyle,
+                    // style: elevatedButtonStyle,
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            titlePadding: const EdgeInsets.all(0.0),
+                            contentPadding: const EdgeInsets.all(0.0),
+                            content: SingleChildScrollView(
+                              child: ColorPicker(
+                                pickerColor: _color,
+                                onColorChanged: changeColor,
+                                colorPickerWidth: 300.0,
+                                pickerAreaHeightPercent: 0.7,
+                                enableAlpha: true,
+                                displayThumbColor: true,
+                                showLabel: true,
+                                paletteType: PaletteType.hsv,
+                                pickerAreaBorderRadius: const BorderRadius.only(
+                                  topLeft: const Radius.circular(2.0),
+                                  topRight: const Radius.circular(2.0),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Text(
+                      'Color',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        // color: Colors.white,
+                      ),
+                    ),
+                    // color: _color,
+                    // textColor: useWhiteForeground(currentColor) ? const Color(0xffffffff) : const Color(0xff000000),
+                  ),
                 ),
 
                 // Update button:
