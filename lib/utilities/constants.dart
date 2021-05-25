e589dd434200a0c26005e5ef54ebd4dfb75aee0d
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-05-25 12:34:40 -0400

    Added the constant DUMMY_URLS with netwprk pictures of meals

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -22,3 +22,20 @@ final Duration animDuration = const Duration(milliseconds: 250);
 // ***********************************************************************************
 
 final double kToolBarHeight = 56.0;
+
+// ***********************************************************************************
+// *                        * * *  D U M M Y  D A T A  * * *                         *
+// ***********************************************************************************
+
+const DUMMY_URLS = const [
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+  'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
+  'https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg',
+  'https://cdn.pixabay.com/photo/2018/03/31/19/29/schnitzel-3279045_1280.jpg',
+  'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
+  'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
+  'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
+  'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
+  'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+];
